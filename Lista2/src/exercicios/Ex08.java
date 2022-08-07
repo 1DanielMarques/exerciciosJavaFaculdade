5af186f1517b2aefe72a4c9b7a12bd42155b2d69
mDanielSE <daniel.marques@edu.unifil.br>
2022-08-07 13:58:14 -0300

    Correção ex08

--- Lista2/src/exercicios/Ex08.java
+++ Lista2/src/exercicios/Ex08.java
@@ -3,15 +3,39 @@ package exercicios;
 public class Ex08 extends AuxScanner {
     public static void main(String[] args) {
 
-        int numero, verifica = 1, aux;
+        int numero, verifica = 1, aux, c = 1, igual = 0;
         int lista[] = new int[5];
 
-        System.out.println("Informe cinco números");
-        for (int i = 0; i <= 4; i++) {
-            numero = sc.nextInt();
-            lista[i] = numero;
+        System.out.println("Informe 5 números");
+
+        while (c != 0) {
+            if (c > 1) {
+                System.out.println("Insira 5 números diferentes");
+            }
+            for (int i = 0; i <= 4; i++) {
+                numero = sc.nextInt();
+                lista[i] = numero;
+            }
+
+            for (int j = 0; j <= 4; j++) {
+                igual = 0;
+                for (int k = 0; k <= 4; k++) {
+                    if (lista[j] == lista[k]) {
+                        igual = igual + 1;
+                        if (igual >= 2) {
+                            c = 2;
+                            j = 4;
+                            break;
+                        }
+                    }
+                }
+            }
+            if (igual < 2) {
+                c = 0;
+            }
+
         }
-        
+
         while (verifica == 1) {
             verifica = 0;
             for (int i = 0; i <= 3; i++) {
@@ -23,8 +47,10 @@ public class Ex08 extends AuxScanner {
                 }
             }
         }
+        System.out.println("-----ORDEM CRESCENTE-----");
         for (int i = 0; i <= 4; i++) {
-            System.out.println(lista[i]);
+            System.out.print(lista[i] + " | ");
         }
+        System.out.println("\n-------------------------");
     }
 }
