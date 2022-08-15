d287cf8d5e00f25b00ca7d720c0250a63fb3855c
mDanielSE <daniel.marques@edu.unifil.br>
2022-08-15 17:37:40 -0300

    alteracao ex04_ex05

--- Lista3/src/ex05.java
+++ Lista3/src/ex05.java
@@ -1,7 +1,8 @@
 public class ex05 extends AuxScanner {
 
     public static void main(String[] args) {
-        double habitantesA, habitantesB, taxaA, taxaB;
+        int habitantesA, habitantesB;
+        double taxaA, taxaB;
         int anos = 0, opcao = 0, verificaA = 0, verificaB = 0;
         do {
             anos = 0;
@@ -14,7 +15,7 @@ public class ex05 extends AuxScanner {
 
             do {
                 System.out.print("Quantidade de habitantes de A: ");
-                habitantesA = sc.nextDouble();
+                habitantesA = sc.nextInt();
                 System.out.print("Taxa de crescimento anual de A: ");
                 taxaA = sc.nextDouble();
                 if (habitantesA <= 1) {
@@ -27,7 +28,7 @@ public class ex05 extends AuxScanner {
             } while (verificaA == 0);
             do {
                 System.out.print("\nQuantidade de habitantes de B: ");
-                habitantesB = sc.nextDouble();
+                habitantesB = sc.nextInt();
                 System.out.print("Taxa de crescimento anual de B: ");
                 taxaB = sc.nextDouble();
                 if (habitantesB <= 1) {
@@ -38,11 +39,11 @@ public class ex05 extends AuxScanner {
                     verificaB = 1;
                 }
             } while (verificaB == 0);
-            taxaA /= 100;
-            taxaB /= 100;
+            taxaA = taxaA /  100;
+            taxaB = taxaB / 100;
             do {
-                habitantesA = habitantesA + (habitantesA * taxaA);
-                habitantesB = habitantesB + (habitantesB * taxaB);
+                habitantesA = (int)(habitantesA + (habitantesA * taxaA));
+                habitantesB = (int)(habitantesB + (habitantesB * taxaB));
                 anos++;
                 if (anos >= 999) {
                     System.out.println("Mais de mil anos ou nunca chegará");
@@ -51,8 +52,12 @@ public class ex05 extends AuxScanner {
             } while (habitantesA < habitantesB);
             if (anos != 999) {
                 if (habitantesA == habitantesB) {
+                    System.out.println("Habitantes de A:" + habitantesA);
+                    System.out.println("Habitantes de B:" + habitantesB);
                     System.out.println("Anos até A igualar B: " + anos);
                 } else {
+                    System.out.println("Habitantes de A:" + habitantesA);
+                    System.out.println("Habitantes de B:" + habitantesB);
                     System.out.println("Anos até A passar B: " + anos);
                 }
             }
