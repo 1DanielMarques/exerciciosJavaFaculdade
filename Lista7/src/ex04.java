31bf3f0a2aa8decf5f5c21d4f2f2cce7d3230fc3
mDanielSE <daniel.marques@edu.unifil.br>
2022-09-10 07:08:13 -0300

    vetor_resultante funcionando para vetor1 e vetor2

--- Lista7/src/ex04.java
+++ Lista7/src/ex04.java
@@ -3,6 +3,7 @@ public class ex04 extends AuxScanner {
     public static int v2[] = new int[10];
     public static int v3[] = new int[20];
     public static int aux = 2;
+    public static int aux2 = 1;
     public static boolean verifica = false;
 
     public static void main(String[] args) {
@@ -11,15 +12,16 @@ public class ex04 extends AuxScanner {
         for (int i = 0; i < v1.length; i++) {
             v1[i] = sc.nextInt();
         }
-        /*System.out.println("Vetor 2");
+        System.out.println("Vetor 2");
         for (int i = 0; i < v2.length; i++) {
             v2[i] = sc.nextInt();
-        }*/
+        }
         v3[0] = v1[0];
         // 1 2 2 3 4
         // 1   2   3
         System.out.println("Vetor resultante");
         vetorResultante1();
+        vetorResultante2();
         for (int i = 0; i < v3.length; i++) {
             System.out.print(v3[i] + " ");
         }
@@ -42,6 +44,29 @@ public class ex04 extends AuxScanner {
                     aux += 2;
                 } else {
                     System.out.println("Aux maior que 18");
+                    break;
+                }
+            }
+
+        }
+    }
+
+    public static void vetorResultante2() {
+        for (int i = 0; i < v2.length; i++) {
+            for (int j = 0; j < v3.length; j++) {
+                if (v2[i] != v3[j]) {
+                    verifica = true;
+                } else {
+                    verifica = false;
+                    break;
+                }
+            }
+            if (verifica) {
+                if (aux2 <= 19) {
+                    v3[aux2] = v2[i];
+                    aux2 += 2;
+                } else {
+                    System.out.println("Aux maior que 19");
                     break;
                 }
             }
