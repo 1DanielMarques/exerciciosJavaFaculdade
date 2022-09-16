fc8010411ded3a2f91e6eb7c37a8fd23c593be8e
mDanielSE <daniel.marques@edu.unifil.br>
2022-09-16 16:00:29 -0300

    Exercicio possivel somar/subtrair/multiplicar

--- exerciciosDaAula/src/main/java/exAula_dia_1509/ex02/Main.java
+++ exerciciosDaAula/src/main/java/exAula_dia_1509/ex02/Main.java
@@ -12,12 +12,28 @@ public class Main {
         int linha = sc.nextInt();
         System.out.print("Colunas:  ");
         int coluna = sc.nextInt();
-
+        int[][] a = new int[linha][coluna];
         System.out.println("Matriz B");
         System.out.print("Linhas:  ");
         linha = sc.nextInt();
         System.out.print("Colunas:  ");
         coluna = sc.nextInt();
+        int[][] b = new int[linha][coluna];
+        if (a.length == b.length && a[0].length == b[0].length) {
+            System.out.println("\nPossível somar/subtrair");
+            System.out.println("Linhas A: " + a.length);
+            System.out.println("Colunas A: " + a[0].length);
+            System.out.println("Linhas B: " + b.length);
+            System.out.println("Colunas B: " + b[0].length);
+        } else if (a[0].length == b.length) {
+            System.out.println("\nPossível multiplicar");
+            System.out.println("Linhas A: " + a.length);
+            System.out.println("Colunas A: " + a[0].length);
+            System.out.println("Linhas B: " + b.length);
+            System.out.println("Colunas B: " + b[0].length);
+        }
+
+
         sc.close();
 
     }
