fb06e47c37820dcdbc1638069cbd18661c94064e
mDanielSE <daniel.marques@edu.unifil.br>
2022-09-17 22:14:45 -0300

    ex04

---
+++ Lista8/src/ex04.java
@@ -0,0 +1,35 @@
+/**
+ * @author Daniel Marques
+ */
+
+public class ex04 extends AuxScanner {
+    public static void main(String[] args) {
+        int[][] mat = new int[4][4];
+        int maior = 0, x = 0, y = 0;
+
+        for (int i = 0; i < mat.length; i++) {
+            for (int j = 0; j < mat[i].length; j++) {
+                System.out.print("Linha " + (i + 1) + " Coluna " + (j + 1) + ": ");
+                mat[i][j] = sc.nextInt();
+                if (i == 0 && j == 0) {
+                    maior = mat[i][j];
+                }
+                if (mat[i][j] > maior) {
+                    maior = mat[i][j];
+                    x = i;
+                    y = j;
+                }
+            }
+        }
+        for (int i = 0; i < mat.length; i++) {
+            System.out.print("| ");
+            for (int j = 0; j < mat[i].length; j++) {
+                System.out.print(mat[i][j] + " | ");
+            }
+            System.out.println();
+        }
+        System.out.println("--MAIOR--");
+        System.out.println("Linha: " + (x + 1) + " Coluna: " + (y + 1));
+
+    }
+}
