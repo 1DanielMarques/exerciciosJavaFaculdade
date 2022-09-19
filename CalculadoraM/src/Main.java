bb90c342025a3e8512dc5f78a1c2ae23ec0e1860
mDanielSE <daniel.marques@edu.unifil.br>
2022-09-19 15:36:34 -0300

    Calculadora Miguel

--- CalculadoraM/src/Main.java
+++ CalculadoraM/src/Main.java
@@ -2,21 +2,26 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static int resultado = 0;
+    public static double resultado = 0;
 
     public static void main(String[] args) throws InterruptedException {
         Scanner sc = new Scanner(System.in);
         char opcao = 'N';
-
+        double n1 = 0;
 
         do {
             System.out.println("CALCULADORA");
+            System.out.println("R: " + resultado);
             calc();
-            int n1 = sc.nextInt();
-            System.out.println(n1);
+            if (opcao != 'C') {
+                System.out.print("N1: ");
+                n1 = sc.nextDouble();
+            } else {
+                n1 = resultado;
+            }
             char operacao = sc.next().charAt(0);
-            System.out.println(n1 + " " + operacao);
-            int n2 = sc.nextInt();
+            System.out.print("N2: ");
+            double n2 = sc.nextDouble();
             switch (operacao) {
                 case '+':
                     resultado = n1 + n2;
@@ -37,6 +42,9 @@ public class Main {
             System.out.println("L - Limpar");
             System.out.println("F - Finalizar");
             opcao = sc.next().charAt(0);
+            if (opcao == 'L') {
+                resultado = 0;
+            }
 
 
         } while (opcao != 'F');
