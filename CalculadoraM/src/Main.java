3cedcc7faca3e0c6008d1ee8a47137950f9bc914
mDanielSE <daniel.marques@edu.unifil.br>
2022-09-18 21:26:02 -0300

    Interface calculadora

---
+++ CalculadoraM/src/Main.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static int resultado = 0;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        char opcao = 'N';
+
+
+        do {
+            System.out.println("CALCULADORA");
+            calc();
+            int n1 = sc.nextInt();
+            System.out.println(n1);
+            char operacao = sc.next().charAt(0);
+            System.out.println(n1 + " " + operacao);
+            int n2 = sc.nextInt();
+            switch (operacao) {
+                case '+':
+                    resultado = n1 + n2;
+                    break;
+                case '-':
+                    resultado = n1 - n2;
+                    break;
+                case '*':
+                    resultado = n1 * n2;
+                    break;
+                case '/':
+                    resultado = n1 / n2;
+                    break;
+            }
+            System.out.println(n1 + " " + operacao + " " + n2 + " = " + resultado);
+            System.out.println("C - Continuar");
+            System.out.println("L - Limpar");
+            System.out.println("F - Finalizar");
+
+
+        } while (opcao != 'F');
+
+        sc.close();
+    }
+
+    public static void calc() {
+        System.out.println("1 2 3 +");
+        System.out.println("4 5 6 -");
+        System.out.println("7 8 9 *");
+        System.out.println("  0   /");
+    }
+
+
+}
