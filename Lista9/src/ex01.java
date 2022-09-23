2b3b97715166daeee490c10cb2451229e9afa67d
mDanielSE <daniel.marques@edu.unifil.br>
2022-09-23 16:44:51 -0300

    ex01 Finalizado

--- Lista9/src/ex01.java
+++ Lista9/src/ex01.java
@@ -1,12 +1,24 @@
 public class ex01 extends AuxScanner {
     public static void main(String[] args) {
         double[][] mat = new double[3][3];
+        double soma = 0;
         for (int i = 0; i < mat.length; i++) {
             for (int j = 0; j < mat[i].length; j++) {
                 System.out.print("Linha " + (i + 1) + " Coluna " + (j + 1) + ": ");
                 mat[i][j] = sc.nextDouble();
             }
         }
+        for (int i = 0; i < mat.length; i++) {
+            System.out.print("| ");
+            for (int j = 0; j < mat[i].length; j++) {
+                System.out.print(mat[i][j]+" | ");
+            }
+            System.out.println();
+        }
+        for (int i = 0; i < mat.length; i++) {
+            soma += mat[i][i];
+        }
+        System.out.println("Soma: " +soma);
 
 
     }
